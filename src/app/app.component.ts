c03a647a69eb6d93d94779bebc819a08ab8f6e22
rinel <rineliniguezsosa@gmail.com>
2023-07-07 00:04:23 -0600

    feat: modificando la propiedad template por templateUrl en app.component.ts
    -la prop templateUrl nos permitira renderizar un archivo html y tan solo le definimos la ruta para acceder al archivo "./app.component.html" por renderizar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hola mundo!</h1>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
