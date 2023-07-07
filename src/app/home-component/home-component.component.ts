150f977d64e50075925d48229046a38e9bc80b8f
rinel <rineliniguezsosa@gmail.com>
2023-07-07 16:37:12 -0600

    feat: estableciendo la interface Housinglocation al  objeto housingLocation para el tipado de las propiedades  de housingLocation

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -7,7 +7,7 @@ import { Housinglocation } from '../housinglocation';
   styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent {
-  housingLocation = {
+  housingLocation : Housinglocation = {
     id: 9999,
     name: 'Test Home',
     city: 'Test city',
