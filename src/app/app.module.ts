da98e790f71b8f109189f261e83fa554b8f01fe7
rinel <rineliniguezsosa@gmail.com>
2023-07-07 13:05:52 -0600

    feat(app.module): importando el componente HomecomponentComponent y establecemos el componente en Ngmodule declarations
    -en la opción declarations estableceremos los components de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule
