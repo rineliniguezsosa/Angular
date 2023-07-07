fb0b79891dd184daec15d23e20ad6b569a178243
rinel <rineliniguezsosa@gmail.com>
2023-07-06 23:57:27 -0600

    feat: modificando la propiedad templateURL por template en app.component.ts
    -la propiedad template devolvera un elemento html h1 con el titulo Hola mundo!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<h1>Hola mundo!</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
