a4d8c84b444952b14fda70b25e96b009cffe314a
rinel <rineliniguezsosa@gmail.com>
2023-07-10 18:01:53 -0600

    feat: importando el componente DetailsComponent y estableciendo el componente en la declaracion del @NgModule para la lectura del componente en la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [ //Aqui establecemos los componentes de la aplicación
     AppComponent,
     HomeComponentComponent,
-    HousingLocationComponent
+    HousingLocationComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
