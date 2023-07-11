d069870afe37717ebf5abbe333b9975876b79b54
rinel <rineliniguezsosa@gmail.com>
2023-07-10 22:18:23 -0600

    feat: importamos el nombre de la clase DetailsComponent para renderizar el html cuando nos localizamos en la ruta details

--- src/app/Approuter.ts
+++ src/app/Approuter.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { DetailsComponent } from './details/details.component';
  
 
 export const Approuter : Routes = [
