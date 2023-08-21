ae731a673d8756e0bae15ccb8035ce30379c4c78
rinel <rineliniguezsosa@gmail.com>
2023-08-20 22:01:26 -0600

    feat: creando la propiedad lastName donde simplemente el valor de entrada del input sera vacio
    -establecimos la validacion de required

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -19,7 +19,8 @@ export class DetailsComponent {
       this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
 
       this.applyForm = this.formservice.group({
-          firstName:['',[Validators.required]]
+          firstName:['',[Validators.required]],
+          lastName:['',[Validators.required]],
       })
     }
 
